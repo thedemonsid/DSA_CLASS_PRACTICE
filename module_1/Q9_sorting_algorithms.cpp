0b0f5083f2d7e89b28a6a887dc2f38fa9ef1a254
thedemonsid <siddheshshrirame@gmail.com>
2023-11-05 11:27:45 +0530

    completing selection sort

--- module_1/Q9_sorting_algorithms.cpp
+++ module_1/Q9_sorting_algorithms.cpp
@@ -27,6 +27,20 @@ void bubble_sort(int  a[],int n){
    }
    cout<<endl;
 }
+//function to selection sort
+void selection_sort(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min_idx]) {
+                min_idx = j;
+            }
+        }
+        swap(a[i], a[min_idx]);
+        display(a, n);
+    }
+    cout << endl;
+}
 
 int main(){
     int n;
