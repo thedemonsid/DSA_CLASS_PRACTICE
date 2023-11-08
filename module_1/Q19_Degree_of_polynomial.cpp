1a50c7ea3dd0229171b14d87473a951de872e63e
Jayesh <jayeshraiger2708@gmail.com>
2023-11-08 16:29:41 +0530

    Solution for question 19

--- module_1/Q19_Degree_of_polynomial.cpp
+++ module_1/Q19_Degree_of_polynomial.cpp
@@ -1,18 +1,21 @@
+/*Finding the highest degree of the polynomial with non - zero coeffecient
+First line will contain T, number of test cases. Then the test cases follow
+First line of each test case contains of a single integer N - the number of terms in the polynomial.
+second line of each test case contains of N spaced integers */
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     int t;
-    cin >> t;
+    cin >> t; // Taking input for no. of test cases//
     while (t--)
     {
         int n, l;
-        cin >> n;
+        cin >> n; // Taking input for highest degree of the polynomials//
         int arr[n];
         for (int i = 1; i <= n; i++)
         {
-            cin >> arr[i];
+            cin >> arr[i]; // Taking input for coeffecient of the ith element//
         }
         for (int i = 1; i <= n; i++)
         {
