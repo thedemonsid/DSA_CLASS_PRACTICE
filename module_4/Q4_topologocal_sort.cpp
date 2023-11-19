ff8f9b9f0580f77dc7fc23d7c36b6176d022840b
thedemonsid <siddheshshrirame@gmail.com>
2023-11-19 19:48:10 +0530

    formating topological sort problemQ4

--- module_4/Q4_topologocal_sort.cpp
+++ module_4/Q4_topologocal_sort.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #include <bits/stdc++.h>
 using namespace std;
@@ -29,38 +29,46 @@ public:
             cout << endl;
         }
     }
-    
-    void DFS(){
-        stack<int>ans;
-        vector<int>visited(n+1,false);
-         for(int i=0;i<=n;i++){
-            if(visited[i]==false) DFS2(visited,arr[i],i,ans); 
-         }
 
-        while(!empty(ans)){
-            cout<<ans.top()<<" ";
+    void DFS()
+    {
+        stack<int> ans;
+        vector<int> visited(n + 1, false);
+        for (int i = 0; i <= n; i++)
+        {
+            if (visited[i] == false)
+                DFS2(visited, arr[i], i, ans);
+        }
+
+        while (!empty(ans))
+        {
+            cout << ans.top() << " ";
             ans.pop();
-        }   
+        }
     }
-    void DFS2(vector<int>&visited,vector<int>a,int source,stack<int>&ans){
-       // cout<<source<<" ";
-        visited[source]=true;
-        for(int i=0;i<a.size();i++){
-            if(visited[a[i]]==false)DFS2(visited,arr[a[i]],a[i],ans);
+    void DFS2(vector<int> &visited, vector<int> a, int source, stack<int> &ans)
+    {
+        // cout<<source<<" ";
+        visited[source] = true;
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (visited[a[i]] == false)
+                DFS2(visited, arr[a[i]], a[i], ans);
         }
         ans.push(source);
         return;
-    }       
-}; 
+    }
+};
 
-int main(){
-  graph_list*a=new graph_list(5);
-  a->addelement(5,0);
-  a->addelement(4,0);
-  a->addelement(5,2);
-  a->addelement(2,3);
-  a->addelement(3,1);
-  a->addelement(4,1);
-  a->display();
-  a->DFS();
+int main()
+{
+    graph_list *a = new graph_list(5);
+    a->addelement(5, 0);
+    a->addelement(4, 0);
+    a->addelement(5, 2);
+    a->addelement(2, 3);
+    a->addelement(3, 1);
+    a->addelement(4, 1);
+    a->display();
+    a->DFS();
 }
